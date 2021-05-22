3a9610239e03b8952d3c3f1a7b41b4563d0d50ec
tmfitra <49491540+tmfitra@users.noreply.github.com>
2021-05-22 08:19:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document) .ready(function(){
+
+    $('.slider').slick({
+        prevArrow:'.prev-btn',
+        nextArrow:'.next-btn',
+        dots:true,
+        appendDots:'.slider-dots'
+    })
+});
+
